65f69125a9e2d108a1d0bfad732bdb896597be6e
hlin <henrylin2008@yahoo.com>
2019-07-07 07:32:08 -0700

    updating UserModel in models & resources

--- Code/resources/user.py
+++ Code/resources/user.py
@@ -19,21 +19,14 @@ class UserRegister(Resource):
 			help="This field cannot be blank."
 	)
 
-
 	def post(self):
 		data = UserRegister.parser.parse_args()
 
 		if UserModel.find_by_username(data['username']): ## if username is not none
 			return {"message": "A user with that username already exist"}, 400
 
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
-
-		query = "INSERT INTO users VALUES (NULL, ?, ?)"
-		cursor.execute(query, (data['username'], data['password']))
-
-		connection.commit()
-		connection.close()
+		user = UserModel(**data) ## data['username'], data['password']
+		user.save_to_db()
 
 		return {"message": "User created successfully."}, 201
 
