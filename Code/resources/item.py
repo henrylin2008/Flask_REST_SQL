c49639617791995c82ddd9a061bef6fd531a1624
hlin <henrylin2008@yahoo.com>
2019-07-09 12:04:49 -0700

    Getting the JWT identity and endpoint and jwt_optional

--- Code/resources/item.py
+++ Code/resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 class Item(Resource):
@@ -66,6 +66,14 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+	@jwt_optional
 	def get(self):
-		return {'items': [item.json() for item in ItemModel.find_all()]}
-		## same as 'items': list(map(lambda x: x.json(), ItemModel.query.all()))
+		user_id = get_jwt_identity()
+		items = [item.json() for item in ItemModel.find_all()]  ## same as 'items': list(map(lambda x: x.json(), ItemModel.query.all()))
+
+		if user_id:
+			return {'items': items}, 200
+		return{
+			'items': [item['name'] for item in items],
+			'message': 'More data available if you log in.'
+		}, 200
