d03b3d4ac923251b4f6584fd8b6aa5a7ffbb6083
hlin <henrylin2008@yahoo.com>
2019-07-10 08:19:32 -0700

    Blacklisting with Flask-JWT-extended: app.py, blacklist.py

--- Code/app.py
+++ Code/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -6,11 +6,14 @@ from security import authenticate, identity ## no longer needed after import JWT
 from resources.user import UserRegister, User, UserLogin, TokenRefresh
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = 'jj' # app.config['JWT_SECRET_KEY']
 api = Api(app)
 
@@ -26,6 +29,10 @@ def add_claims_to_jwt(identity):
         return {'is_admin': True}
     return {'is_admin': False}
 
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST
+
 @jwt.expired_token_loader #expired token, usually every 5 mins,
 def exired_token_callback():
     return jsonify({
@@ -33,14 +40,14 @@ def exired_token_callback():
         'error': 'token_expired'
     }), 401
 
-@jwt_invalid_token_loader
+@jwt.invalid_token_loader
 def invalid_token_callback(error):
     return jsonify({
         'description': 'Signature verification failed.',
         'error': 'invalid_token'
     }), 401
 
-@jwt_unauthorized_loader ## called when no jwt sent
+@jwt.unauthorized_loader ## called when no jwt sent
 def missing_token_callback(error):
     return jsonify({
         'description': 'Request does not contain an access token.',
@@ -54,7 +61,7 @@ def token_not_fresh_callback():
         'error': 'fresh_token_required'
     }), 401
 
-@jwt.revoked_token_loader():
+@jwt.revoked_token_loader
 def revoked_token_callback():
     return jsonify({
         'description': 'The token has been revoked',
